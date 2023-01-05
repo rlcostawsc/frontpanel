a1832e72fb63da26b7f2290f5c8987244a82841d
Rodrigo Wanzeler Dos Santos Costa <43542851+rlcostawsc@users.noreply.github.com>
2023-01-05 17:54:10 -0300

    developer do header

---
+++ src/Btn_app.js
@@ -0,0 +1,25 @@
+function Profile () {
+	return(
+		<div>
+			<button>ok</button>
+		</div>
+		);
+}
+export function Titulo(){
+	return(
+		<div>
+			<h1>Branch</h1>
+		</div>);
+}
+
+export function Gallery(){
+	return(
+		<section>
+			<h1>Botões app</h1>
+			<Profile/>
+			<Profile/>
+			<Profile/>
+		</section>
+		);
+}
+
